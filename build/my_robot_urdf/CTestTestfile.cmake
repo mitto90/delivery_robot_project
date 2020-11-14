757ba4923aa49f7b387e1b6d145046a17192090a
Jaime Gonzalez <jegonzalez17@yahoo.com>
2020-11-14 11:22:31 -0800

    first commit

---
+++ build/my_robot_urdf/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mitto90/delivery_robot_project/src/my_robot_urdf
+# Build directory: /home/mitto90/delivery_robot_project/build/my_robot_urdf
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
